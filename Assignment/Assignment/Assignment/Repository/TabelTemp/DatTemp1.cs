e5d9e313b5dcb97fd2666744c600e9c58c4dd2fd
ArdiDramilio <ardidramilio83@gmail.com>
2020-09-25 13:34:11 +0700

    Hapus File

--- Assignment/Assignment/Assignment/Repository/TabelTemp/DatTemp1.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Assignment.Repository
-{
-    public class DatTemp1
-    {
-        public string ProductTypeID { get; internal set; }
-        public string ProductTypeName { get; internal set; }
-        public string ProductTypeDesc { get; internal set; }
-    }
-}
